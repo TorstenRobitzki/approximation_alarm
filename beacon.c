0f64427948954526337b7efc02633b0974df1846
Torsten Robitzki <Torsten@Robitzki.de>
2016-10-05 19:50:24 +0200

    correct application ROM and RAM addresses and sizes; start advertising

--- beacon.c
+++ beacon.c
@@ -1,6 +1,8 @@
 #include <nrf.h>
+#include <nrf_sdm.h>
 #include <app_error.h>
 #include <ble_gap.h>
+#include <softdevice_handler.h>
 
 #include <string.h>
 
@@ -11,9 +13,85 @@
 #define SLAVE_LATENCY                    0                                          /**< Slave latency. */
 #define CONN_SUP_TIMEOUT                 MSEC_TO_UNITS(4000, UNIT_10_MS)            /**< Connection supervisory timeout (4 seconds). */
 
+#define ADVERTING_INTERVAL               MSEC_TO_UNITS(50, UNIT_0_625_MS)
+
+#define CENTRAL_LINK_COUNT              0                                           /**< The number of central links used by the application. When changing this number remember to adjust the RAM settings. */
+#define PERIPHERAL_LINK_COUNT           1                                           /**< The number of peripheral links used by the application. When changing this number remember to adjust the RAM settings. */
+#define VENDOR_SPECIFIC_UUID_COUNT      1                                           /**< The number of vendor specific UUIDs used by this example. */
+
+// clock settings
+#define NRF_CLOCK_LFCLKSRC  { \
+    .source        = NRF_CLOCK_LF_SRC_XTAL,            \
+    .rc_ctiv       = 0,                                \
+    .rc_temp_ctiv  = 0,                                \
+    .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM \
+}
+
+void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
+{
+    for ( ;; )
+        ;
+}
+
 void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
 {
-    app_error_handler(0xDEADBEEF, line_num, p_file_name);
+    for ( ;; )
+        ;
+}
+
+//    app_error_handler(0xDEADBEEF, line_num, p_file_name);
+
+static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
+{
+    /*
+    dm_ble_evt_handler(p_ble_evt);
+    ble_db_discovery_on_ble_evt(&m_ble_db_discovery, p_ble_evt);
+    ble_conn_params_on_ble_evt(p_ble_evt);
+    ble_ancs_c_on_ble_evt(&m_ancs_c, p_ble_evt);
+    bsp_btn_ble_on_ble_evt(p_ble_evt);
+    on_ble_evt(p_ble_evt);
+    ble_advertising_on_ble_evt(p_ble_evt);
+    */
+}
+
+static void sys_evt_dispatch(uint32_t sys_evt)
+{
+    // pstorage_sys_event_handler(sys_evt);
+    // ble_advertising_on_sys_evt(sys_evt);
+}
+
+
+static void ble_stack_init(void)
+{
+    uint32_t err_code;
+
+    nrf_clock_lf_cfg_t clock_lf_cfg = NRF_CLOCK_LFCLKSRC;
+
+    // Initialize the SoftDevice handler module.
+    SOFTDEVICE_HANDLER_INIT(&clock_lf_cfg, NULL);
+
+    ble_enable_params_t ble_enable_params;
+    err_code = softdevice_enable_get_default_config(CENTRAL_LINK_COUNT,
+                                                    PERIPHERAL_LINK_COUNT,
+                                                    &ble_enable_params);
+    APP_ERROR_CHECK(err_code);
+
+    ble_enable_params.common_enable_params.vs_uuid_count = VENDOR_SPECIFIC_UUID_COUNT;
+
+    //Check the ram settings against the used number of links
+    CHECK_RAM_START_ADDR(CENTRAL_LINK_COUNT,PERIPHERAL_LINK_COUNT);
+
+    // Enable BLE stack.
+    err_code = softdevice_enable(&ble_enable_params);
+    APP_ERROR_CHECK(err_code);
+
+    // Register with the SoftDevice handler module for BLE events.
+    err_code = softdevice_ble_evt_handler_set(ble_evt_dispatch);
+    APP_ERROR_CHECK(err_code);
+
+    // Register with the SoftDevice handler module for System events.
+    err_code = softdevice_sys_evt_handler_set(sys_evt_dispatch);
+    APP_ERROR_CHECK(err_code);
 }
 
 static void gap_params_init(void)
@@ -38,9 +116,26 @@ static void gap_params_init(void)
     APP_ERROR_CHECK( sd_ble_gap_ppcp_set(&gap_conn_params) );
 }
 
+static void advertising_start(void)
+{
+    static const ble_gap_adv_params_t params = {
+        .interval = ADVERTING_INTERVAL
+    };
+
+    APP_ERROR_CHECK( sd_ble_gap_adv_start( &params ) );
+}
+
 int main()
 {
+    ble_stack_init();
     gap_params_init();
+
+    advertising_start();
+
+    for (;;)
+    {
+        APP_ERROR_CHECK( sd_app_evt_wait() );
+    }
 
     return 0;
 }
